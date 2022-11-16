08839e9e85c004d49258f49739bc300fa9665365
DIVYANSH081998 <ydivyansh4@gmail.com>
2022-11-16 11:44:52 +0530

    Add files via upload

---
+++ BrickBreaker.java
@@ -0,0 +1,15 @@
+package  game;
+import javax.swing.JFrame;
+
+public class BrickBreaker {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+}
